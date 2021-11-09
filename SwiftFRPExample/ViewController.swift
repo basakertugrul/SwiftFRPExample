254f31de34441d67d223752705af756f8aadf409
basakertugrul <61563492+basakertugrul@users.noreply.github.com>
2021-11-09 13:09:08 +0300

    “Commit”

--- SwiftFRPExample/ViewController.swift
+++ SwiftFRPExample/ViewController.swift
@@ -6,9 +6,14 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signUpButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
