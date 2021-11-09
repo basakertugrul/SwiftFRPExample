15f74986627674015234a159e75e28a7f493552d
basakertugrul <61563492+basakertugrul@users.noreply.github.com>
2021-11-09 13:32:43 +0300

    “Commit”

--- SwiftFRPExample/ViewController.swift
+++ SwiftFRPExample/ViewController.swift
@@ -14,11 +14,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var signUpButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        emailTextField.rx.text.map { text in
+            
+            let isEmailValid = self.isValidEmail(text!)
+            return isEmailValid
+        }
+        .bind(to: self.signUpButton.rx.isEnabled)
     }
 
+    func isValidEmail(_ email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: email)
+    }
 
 }
 
